823e5c2122fe00b033cf3ce944603d2a901439b1
charleshbl <101838845+charleshbl@users.noreply.github.com>
2022-06-08 20:33:04 -0400

    Ajoutez des fichiers projet.

---
+++ LibrairieConversionNote/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,33 @@
+﻿namespace LibrairieConversionNote;
+
+public class ConversionAlphabetiqueToNote
+{
+    private const string _DO = "Do";
+    private const string _RE = "RÉ";
+    private const string _MI = "MI";
+    private const string _FA = "FA";
+    private const string _SOL = "SOL";
+    private const string _LA = "LA";
+    private const string _SI = "SI";
+    
+    
+    public string Transformer(string Lettre)
+    {
+        switch ((Lettre)
+)
+        {
+            case "A": return _LA;
+            case "B": return _SI;
+            case "C": return _DO;
+            case "D": return _RE;
+            case "E": return _MI;
+            case "F": return _FA;
+            case "G": return _SOL;
+
+            default:
+                return "La note n'existe pas";
+
+        }
+
+    }
+}
---
+++ SDK_Laboratoire1/Program.cs
@@ -0,0 +1,19 @@
+﻿using LibrairieConversionNote;
+
+
+
+ConversionAlphabetiqueToNote conversionAlphabetiqueToNote=new ConversionAlphabetiqueToNote();
+
+do
+{
+    Console.WriteLine("Bonjours Quelle conversion voulez-vous faire:");
+   public string choix = Console.ReadLine() ;
+} while (key.Key != ConsoleKey.Enter);
+
+while (true)
+{
+    
+    Console.WriteLine($@"Entrer une lettre entre A et G 
+ {conversionAlphabetiqueToNote.Transformer(Console.ReadKey(intercept:true).KeyChar.ToString().ToUpper())}");
+
+}
